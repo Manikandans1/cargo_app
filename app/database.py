0fbf2389c7ae9edb2ed1ca79d6ce3859f3729789
Manikandans1 <manimp377@gmail.com>
2024-10-05 10:22:35 +0530

    data base modified

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Updated database URL for PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://Embassy:Embassy1@localhost/cargoapp"
+# Get the database URL from the environment variable
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# If the database URL is not available, raise an error
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("No DATABASE_URL set for the application")
 
 # Create the PostgreSQL engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -12,7 +16,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # Session for interacting with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class for our ORM models
+# Base class for ORM models
 Base = declarative_base()
 
 # Dependency for database sessions
@@ -22,6 +26,37 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+
+
+
+
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+# # Updated database URL for PostgreSQL
+# SQLALCHEMY_DATABASE_URL = "postgresql://Embassy:Embassy1@localhost/cargoapp"
+
+# # Create the PostgreSQL engine
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# # Session for interacting with the database
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# # Base class for our ORM models
+# Base = declarative_base()
+
+# # Dependency for database sessions
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
 
 
 
